d4d04f275f617694e984ccc817b6717b76954345
admin <admin>
2021-08-02 17:10:48 -0700

    Saved recipe: DKU_PREDICTIVE_MAINTENANCE.compute_python_output

---
+++ recipes/compute_python_output.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+asset_modelisation_table = dataiku.Dataset("Asset_modelisation_table")
+asset_modelisation_table_df = asset_modelisation_table.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+
+
+
+python_output_df = asset_modelisation_table_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+python_output = dataiku.Dataset("python_output")
+python_output.write_with_schema(python_output_df)
